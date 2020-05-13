ad9de8370691679f45dc5a590336af02e28b0db8
Artem Kulyhin <63202128+akulyhin@users.noreply.github.com>
2020-05-14 00:04:38 +0300

    add button go 2 top

--- hw-5/js/main.js
+++ hw-5/js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
-$('.theme-button').click(function(){
+$('.theme-button').click(function() {
     $('.light-theme').toggleClass('dark-theme');
     });
 });
+
+
+
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    
+         /*Если сделали скролл на 100px задаём новый класс для header*/
+    if(height > 200){
+    $('.back-2-top').addClass('chown');
+    } else{
+         /*Если меньше 100px удаляем класс для header*/
+    $('.back-2-top').removeClass('chown');
+    }
+    });
